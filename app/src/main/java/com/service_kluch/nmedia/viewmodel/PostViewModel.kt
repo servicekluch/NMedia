fd1754122c70dec3dbfbf5aa5add74a37f88d90c
servicekluch <97802322+servicekluch@users.noreply.github.com>
2022-04-25 13:25:21 +0400

    Update PostViewModel.kt
    teacher's advice

--- app/src/main/java/com/service_kluch/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/service_kluch/nmedia/viewmodel/PostViewModel.kt
@@ -1,13 +1,10 @@
 package com.service_kluch.nmedia
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
 class PostViewModel : ViewModel() {
     private val repository = PostRepositoryInMemory()
-    val data: LiveData<Post>
-
-    get() = repository.data
+    val data by repository::data
 
     fun like() {
         repository.like()
