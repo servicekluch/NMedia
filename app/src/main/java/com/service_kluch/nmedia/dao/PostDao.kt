9629eb1b53d8ff02b37163cb7ac3c1ec47b59a7d
samara04 <samara04@yandex.ru>
2022-05-13 20:33:29 +0400

    SQlite

---
+++ app/src/main/java/com/service_kluch/nmedia/dao/PostDao.kt
@@ -0,0 +1,13 @@
+package com.service_kluch.nmedia.dao
+
+import com.service_kluch.nmedia.dto.Post
+
+interface PostDao {
+    fun getAll(): List<Post>
+    fun likeById(id: Long)
+    fun shareById(id: Long)
+    fun removeById(id: Long)
+    fun save(post: Post): Post
+    fun cancelEditing(post: Post)
+    fun isVideo (post: Post): Boolean
+}
